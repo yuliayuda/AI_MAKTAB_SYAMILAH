a5c4a41074188bbdc0ca6e8f81b8aebb3946effd
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-25 03:45:26 +0700

    Update qa_module.py

--- modules/qa_module.py
+++ modules/qa_module.py
@@ -1,7 +1,8 @@
 from transformers import pipeline
 
 # Inisialisasi model question answering (BERT, AraBERT, atau lainnya)
-qa_model = pipeline("question-answering", model="CAMeL-Lab/bert-base-arabic-camelbert-da")
+qa_model = pipeline("question-answering", model="CAMeL-Lab/bert-base-arabic-camelbert-da", device=0)  # device=0 for GPU)
+
 
 def get_answer(question, context):
     response = qa_model(question=question, context=context)
