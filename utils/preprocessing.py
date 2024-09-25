b094628b60c860b669f10e0cdc6399a083684b9d
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-25 23:02:06 +0700

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,10 +1,8 @@
-import re
 import pandas as pd
 from tqdm import tqdm
 from camel_tools.utils.charmap import CharMapper
 from nltk.tokenize import word_tokenize
 import nltk
-from sklearn.model_selection import train_test_split  # Tambahkan ini
 
 # Mengunduh tokenizer bahasa Arab
 nltk.download('punkt')
@@ -29,15 +27,15 @@ def arabic_preprocessing(texts):
 def load_data_in_chunks(file_path, chunksize=1000):
     print("Membaca dataset...")
     
+    # Menghitung total baris di CSV
+    total_rows = sum(1 for _ in open(file_path)) - 1
     chunk_results = []
-    
-    for chunk in tqdm(pd.read_csv(file_path, chunksize=chunksize), desc="Memproses chunk", unit="chunk"):
+
+    # Menggunakan tqdm untuk memproses setiap chunk
+    for chunk in tqdm(pd.read_csv(file_path, chunksize=chunksize), total=(total_rows // chunksize), desc="Memproses chunk", unit="chunk"):
+        # Proses teks dalam chunk
         processed_texts = arabic_preprocessing(chunk['text'])
         chunk_results.extend(processed_texts)
 
     print("\nSemua chunk diproses.")
     return pd.DataFrame(chunk_results, columns=['text'])  # Mengembalikan DataFrame
-
-def split_data(df, test_size=0.2, random_state=42):
-    train_data, test_data = train_test_split(df, test_size=test_size, random_state=random_state)
-    return train_data, test_data
