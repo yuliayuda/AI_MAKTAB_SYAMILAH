e86e1c7315b80b623eff6c9f53c8bf2bd8d558bd
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-25 07:11:56 +0700

    Update train_model.py

--- training/train_model.py
+++ training/train_model.py
@@ -1,22 +1,11 @@
-from transformers import Trainer, TrainingArguments
-from datasets import load_dataset
+# training/train_model.py
+from models.BERT import train_model as train_bert
+from models.LSTM import train_model as train_lstm
+from models.Transformer import train_model as train_transformer
 
-def train_model(model, dataset):
-    training_args = TrainingArguments(
-        output_dir='./results',
-        evaluation_strategy="epoch",
-        learning_rate=2e-5,
-        per_device_train_batch_size=16,
-        num_train_epochs=3,
-        weight_decay=0.01,
-    )
-
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=dataset['train'],
-        eval_dataset=dataset['validation'],
-    )
-
-    trainer.train()
-    return model
+def train_all_models(data):
+    bert_model = train_bert(data)
+    lstm_model = train_lstm(data)
+    transformer_model = train_transformer(data)
+    
+    return bert_model, lstm_model, transformer_model
