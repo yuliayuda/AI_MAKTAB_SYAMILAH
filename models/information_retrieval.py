7985e970eb8c00dbb961916e96eacb7a01ccb93a
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-25 08:36:49 +0700

    Create information_retrieval.py

---
+++ models/information_retrieval.py
@@ -0,0 +1,13 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from camel_tools.tokenizers.word import simple_word_tokenize
+
+def retrieve_information(query, data):
+    vectorizer = TfidfVectorizer(tokenizer=simple_word_tokenize)
+    tfidf_matrix = vectorizer.fit_transform(data['text'])
+    
+    query_vec = vectorizer.transform([query])
+    similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
+    
+    top_index = similarities.argmax()
+    return data.iloc[top_index]['text']
