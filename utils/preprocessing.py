a6975c2f2ecbfa642ae964252d9899b163d35d75
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-25 22:54:36 +0700

    Update preprocessing.py

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -9,7 +9,6 @@ import nltk
 nltk.download('punkt')
 
 def arabic_preprocessing(texts):
-    # Menggunakan tqdm untuk menampilkan progres pemrosesan teks
     tqdm.pandas(desc="Memproses teks")
     
     def clean_text(text):
@@ -29,24 +28,11 @@ def arabic_preprocessing(texts):
 def load_data_in_chunks(file_path, chunksize=1000):
     print("Membaca dataset...")
     
-    # Menghitung total baris di CSV
-    total_rows = sum(1 for _ in open(file_path)) - 1  
     chunk_results = []
-
-    # Menggunakan tqdm untuk memproses setiap chunk
-    for chunk in tqdm(pd.read_csv(file_path, chunksize=chunksize), total=(total_rows // chunksize), desc="Memproses chunk", unit="chunk"):
-        # Proses teks dalam chunk
+    
+    for chunk in tqdm(pd.read_csv(file_path, chunksize=chunksize), desc="Memproses chunk", unit="chunk"):
         processed_texts = arabic_preprocessing(chunk['text'])
         chunk_results.extend(processed_texts)
 
     print("\nSemua chunk diproses.")
-    return chunk_results
-
-
-
-# Contoh penggunaan
-file_path = "/kaggle/input/arabic-library/my_csv.csv"  # Ganti dengan path dataset Anda
-processed_data = load_data_in_chunks(file_path, chunksize=1000)
-
-# Menyimpan hasil ke file CSV baru jika diperlukan
-pd.DataFrame(processed_data, columns=['processed_text']).to_csv("data/processed/processed_data.csv", index=False)
+    return pd.DataFrame(chunk_results, columns=['text'])  # Mengembalikan DataFrame
