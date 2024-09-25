88d7061fa4703ca090b70a7d745d59cbb6caf05f
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-25 07:18:23 +0700

    Update LSTM.py

--- models/LSTM.py
+++ models/LSTM.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
 class TextDataset(Dataset):
