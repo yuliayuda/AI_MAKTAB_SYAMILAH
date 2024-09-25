14a1d74b518401c150b7dd317274e998ee23a0b2
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-25 08:08:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
 # main.py
 
-# Impor pustaka yang diperlukan
 import pandas as pd
-import logging  # Untuk logging
-
-# Impor dari modul Anda
+import logging
 from data.preprocessing_data import load_data, preprocess_data
 from training.train_model import train_all_models
 from evaluation.evaluate_model import evaluate_model
-from logs.logging import setup_logging
 
 def main():
-    # Setup logging
-    setup_logging()
-    
+    # Mengatur konfigurasi logging
+    logging.basicConfig(level=logging.INFO)
+
     # Proses memuat dan mempersiapkan data
     logging.info("Memulai proses pemrosesan data...")
-    data = load_data('/kaggle/input/ar-id-en-stop/ar_stop_word.json')  # Ganti dengan path data Anda
-    processed_data = preprocess_data(data)
-    processed_file_path = 'data/processed/processed_dataset.csv'  # Pastikan direktori ini ada
-    processed_data.to_csv(processed_file_path, index=False)  # Simpan sebagai CSV
+    data = preprocess_data('/kaggle/input/ar-id-en-stop/ar_stop_word.json')  # Ganti dengan path data Anda
     logging.info("Pemrosesan data selesai.")
+
+    # Inisialisasi models
+    models = None
 
     # Memulai pelatihan model
     logging.info("Mulai proses pelatihan...")
     try:
-        models = train_all_models(processed_data)  # Memanggil fungsi pelatihan
+        models = train_all_models(data)  # Memanggil fungsi pelatihan
         logging.info("Pelatihan selesai untuk semua model.")
     except Exception as e:
         logging.error(f"Terjadi kesalahan saat pelatihan: {e}")
 
-    # Evaluasi model
-    evaluation_results = evaluate_model(models, processed_data)  # Sesuaikan dengan parameter yang tepat
-    logging.info(f"Hasil evaluasi: {evaluation_results}")
+    # Hanya evaluasi jika models tidak None
+    if models is not None:
+        logging.info("Memulai evaluasi model...")
+        evaluation_results = evaluate_model(models, data)  # Sesuaikan dengan parameter yang tepat
+        logging.info("Hasil evaluasi: {}".format(evaluation_results))
+    else:
+        logging.warning("Model tidak dilatih, tidak ada hasil evaluasi yang dapat dilakukan.")
 
 if __name__ == "__main__":
     main()
