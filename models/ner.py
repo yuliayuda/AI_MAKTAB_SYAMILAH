48002dc60e23deeb1fce1a196fa83c9d3826ab41
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-25 22:53:23 +0700

    Update ner.py

--- models/ner.py
+++ models/ner.py
@@ -1,7 +1,6 @@
 from transformers import pipeline
 
 def named_entity_recognition(text):
-    # Menggunakan model pre-trained untuk NER bahasa Arab
     ner_model = pipeline("ner", model="asafaya/bert-base-arabic")
     ner_results = ner_model(text)
 
