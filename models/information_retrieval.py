d99f6610df8cae32dce7a3e1e839da26f60070ef
yulia yuda <179325406+yuliayuda@users.noreply.github.com>
2024-09-27 23:24:59 +0700

    Update information_retrieval.py

--- models/information_retrieval.py
+++ models/information_retrieval.py
@@ -4,10 +4,10 @@ from camel_tools.tokenizers.word import simple_word_tokenize
 
 def retrieve_information(query, data):
     vectorizer = TfidfVectorizer(tokenizer=simple_word_tokenize)
-    tfidf_matrix = vectorizer.fit_transform(data['text'])
+    tfidf_matrix = vectorizer.fit_transform(data[text])
     
     query_vec = vectorizer.transform([query])
     similarities = cosine_similarity(query_vec, tfidf_matrix).flatten()
     
     top_index = similarities.argmax()
-    return data.iloc[top_index]['text']
+    return data.iloc[top_index][text]
